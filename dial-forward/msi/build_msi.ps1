0c117ce8da7af04152766dada8663e53bfb32111
123asxcqasdc <105458065+123asxcqasdc@users.noreply.github.com>
2026-08-29 19:18:44 +0300

    Codepage on Product (not Package)

--- dial-forward/msi/build_msi.ps1
+++ dial-forward/msi/build_msi.ps1
@@ -30,10 +30,10 @@ if ($LASTEXITCODE -ne 0) { throw "heat failed" }
 $Template = @"
 <?xml version="1.0" encoding="UTF-8"?>
 <Wix xmlns="http://schemas.microsoft.com/wix/2006/wi">
-  <Product Id="*" Name="Dial Forward" Language="1033" Version="$ProductVersion"
+  <Product Id="*" Name="Dial Forward" Language="1033" Version="$ProductVersion" Codepage="65001"
            Manufacturer="Dial Forward"
            UpgradeCode="D1A1F000-0000-4000-9000-A1F0D1A1F000">
-    <Package InstallerVersion="500" Compressed="yes" InstallScope="perUser" Codepage="65001"
+    <Package InstallerVersion="500" Compressed="yes" InstallScope="perUser"
              Description="Dial Forward - P2P calls over Telegram"
              Manufacturer="Dial Forward" />
     <MajorUpgrade DowngradeErrorMessage="A newer version is already installed." />
